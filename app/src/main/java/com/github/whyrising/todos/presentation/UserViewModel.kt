e3e35df031fc5fc76d4cf1904988202326a25d13
Yahya <47183730+whyrising@users.noreply.github.com>
2022-03-11 12:06:59 +0000

    Separate User from view by introducing UserViewModel

---
+++ app/src/main/java/com/github/whyrising/todos/presentation/UserViewModel.kt
@@ -0,0 +1,10 @@
+package com.github.whyrising.todos.presentation
+
+import com.github.whyrising.todos.gateway.User
+
+data class UserViewModel(private val user: User) {
+    val id: String = user.id
+    val fullName: String = user.name
+    val username: String = "#${user.username}"
+    val email: String = user.email
+}
